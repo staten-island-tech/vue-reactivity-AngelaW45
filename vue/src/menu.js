dc9e1d0201925d21f4c880490b821c3db4b0c2f8
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-03-10 10:16:17 -0500

    2

---
+++ vue/src/menu.js
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const items = [
+  {
+    Name: "Watermelon",
+    description: "",
+  },
+];
